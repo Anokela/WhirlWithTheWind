aa0cfd4ca623528692d1f50cceda5b21c96854bc
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-09-28 12:31:06 +0300

    Player can now collect lightpoints from the map

--- Assets/Scripts/LightPointCollect.cs
+++ Assets/Scripts/LightPointCollect.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class LightPointCollect : MonoBehaviour
 {
+    private int lightPoints;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lightPoints = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(lightPoints);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Hep");
+        Destroy(col.gameObject);
+        lightPoints += 1; 
     }
 }
