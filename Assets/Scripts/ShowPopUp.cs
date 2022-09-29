962d61bc2a0909d52c0c6331e796f72b23b5a9bf
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-09-29 13:32:39 +0300

    Gameover popup when character touches ground

---
+++ Assets/Scripts/ShowPopUp.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPopUp : MonoBehaviour
+{
+    private GameObject Panel;
+
+    void Start()
+    {
+        Panel = GameObject.Find("PopUp");
+        Panel.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D c2d)
+    {
+        if (c2d.CompareTag("Player"))
+        {
+            Invoke("showPanel", 0);
+        }
+    }
+
+    public void showPanel()
+    { 
+        Panel.SetActive(true);
+    }
+}
