ed48ab2463b472f215be8b24ef7cbb49ae389141
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-09-30 15:26:56 +0300

    started implementing wind obstacles

---
+++ Assets/Scripts/WindObstacle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindObstacle : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    public Vector3 windDirection;
+    public float windForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("PlayerChar");
+        rb = player.GetComponent<Rigidbody2D>();
+        Debug.Log(rb);
+    }
+
+    // Update is called once per frame
+   /* void Update()
+    {
+        rb.AddForce(new Vector3(windDirection.x * Time.deltaTime * windForce, 0), ForceMode2D.Force);
+    }*/
+
+    void OnTriggerStay2D(Collider2D c2d)
+    {
+        if (c2d.CompareTag("Player"))
+        {
+            rb.AddForce(Vector3.right * windForce, ForceMode2D.Force);
+            // rb.AddForce(new Vector3(windDirection.x * Time.deltaTime * windForce, 0), ForceMode2D.Force);
+            Debug.Log("Testi");
+        }
+    }
+}
