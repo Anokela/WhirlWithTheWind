d3aefd88c4a872af51c5f52a71e9cc92aa73ec9c
niko <n0rani00@students.oamk.fi>
2022-10-20 14:57:49 +0300

    beetle and bird

--- Assets/Scripts/BirdBehaviour.cs
+++ Assets/Scripts/BirdBehaviour.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class BirdBehaviour : MonoBehaviour
 {
 
-     public Vector3 pos1 = new Vector3(-1,-2,0);
-     public Vector3 pos2 = new Vector3(1,0,0);
+    public Vector3 pos1;
+    public Vector3 pos2;
      public float speed = 1.0f;
-    
-    void Update() {
-         transform.position = Vector3.Lerp (pos1, pos2, Mathf.PingPong(Time.time*speed, 1.0f));
-             if (transform.position = pos2)
-             {
-               SpriteRenderer.flipX;
-               }
-                if (transform.position = pos1)
-             {
-               SpriteRenderer.flipX;
-               }
-               
-     }
+    public GameObject bird;
+    private bool m_FacingRight = true;
+
+    void Start()
+    {
+        bird = GameObject.FindGameObjectWithTag("Bird");
+    }
+    void FixedUpdate() {
+         transform.position = Vector3.Lerp (pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
+
+        if (bird.transform.position == pos2)
+        {
+            Flip();
+        }
+        if (bird.transform.position == pos1)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+       
+        m_FacingRight = !m_FacingRight;
+
+     
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
