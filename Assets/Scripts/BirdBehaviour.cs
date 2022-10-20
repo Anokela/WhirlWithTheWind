7de2bb4525154cd9d8a961275bfdb92947ec3ddf
Ville15 <n0luvi00@students.oamk.fi>
2022-10-20 16:38:13 +0300

    Revert "Pulled content"
    This reverts commit cafa6d3e2bc114cc0133d3b5fdf09f16ff546f38, reversing
    changes made to 2d063ae8d14dfdb5f33482c5bd4ba5f0d24988bd.

--- Assets/Scripts/BirdBehaviour.cs
+++ Assets/Scripts/BirdBehaviour.cs
@@ -5,37 +5,11 @@ using UnityEngine;
 public class BirdBehaviour : MonoBehaviour
 {
 
-    public Vector3 pos1;
-    public Vector3 pos2;
+     public Vector3 pos1 = new Vector3(-1,-2,0);
+     public Vector3 pos2 = new Vector3(1,0,0);
      public float speed = 1.0f;
-    public GameObject bird;
-    private bool m_FacingRight = true;
-
-    void Start()
-    {
-        bird = GameObject.FindGameObjectWithTag("Bird");
-    }
-    void FixedUpdate() {
-         transform.position = Vector3.Lerp (pos1, pos2, Mathf.PingPong(Time.time * speed, 1.0f));
-
-        if (bird.transform.position == pos2)
-        {
-            Flip();
-        }
-        if (bird.transform.position == pos1)
-        {
-            Flip();
-        }
-    }
-
-    void Flip()
-    {
-       
-        m_FacingRight = !m_FacingRight;
-
-     
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
+    
+    void Update() {
+         transform.position = Vector3.Lerp (pos1, pos2, Mathf.PingPong(Time.time*speed, 1.0f));
+     }
 }
