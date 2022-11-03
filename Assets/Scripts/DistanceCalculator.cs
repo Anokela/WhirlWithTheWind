07473adc71407aad5c6a9e2080468556994a8c8d
Aleksi Ervasti <n0eral00@students.oamk.fi>
2022-11-03 15:32:42 +0200

    timestopper tag

--- Assets/Scripts/DistanceCalculator.cs
+++ Assets/Scripts/DistanceCalculator.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using String = System.String;
 
 public class DistanceCalculator : MonoBehaviour
 {
     public TextMeshProUGUI distanceText;
     private float roundedDistance;
+    public GameObject pc;
     // Update is called once per frame
     void Update()
     {
