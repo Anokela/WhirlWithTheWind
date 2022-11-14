44018b66ad717f93691e4f171a79eb21e5b04714
Aleksi Ervasti <n0eral00@students.oamk.fi>
2022-11-14 15:19:16 +0200

    background stuff

--- Assets/Scripts/BgManager.cs
+++ Assets/Scripts/BgManager.cs
@@ -8,26 +8,42 @@ public class BgManager : MonoBehaviour
     public GameObject cloudsBg;
     public GameObject canopyBg;
     public GameObject forestBg;
+    public GameObject treeTopsBg;
+    public GameObject treeTrunksBg;
     // Start is called before the first frame update
     void Start()
     {
         canopyBg.SetActive(false);
         forestBg.SetActive(false);
+        treeTopsBg.SetActive(false);
+        treeTrunksBg.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(PlayerInfo.Distance > 25)
+        if (PlayerInfo.Distance > 15)
+        {
+            treeTopsBg.SetActive(true);
+
+        }
+        if (PlayerInfo.Distance > 100)
         {
             skyBg.SetActive(false);
             cloudsBg.SetActive(false);
             canopyBg.SetActive(true);
+            treeTopsBg.SetActive(false);
+            treeTrunksBg.SetActive(true);
+
         }
-        if (PlayerInfo.Distance > 50)
+        if (PlayerInfo.Distance > 140)
         {
             canopyBg.SetActive(false);
             forestBg.SetActive(true);
+        }
+        if (PlayerInfo.Distance > 160)
+        {
+           
         }
     }
 }
