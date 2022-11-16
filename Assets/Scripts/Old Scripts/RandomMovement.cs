f88bdaa553d4ac4e437d03b029bea17047931302
Ville15 <n0luvi00@students.oamk.fi>
2022-11-16 14:05:11 +0200

    box changes

--- Assets/Scripts/Old Scripts/RandomMovement.cs
+++ Assets/Scripts/Old Scripts/RandomMovement.cs
@@ -7,28 +7,35 @@ public class RandomMovement : MonoBehaviour
     public float speed;
     private float waitTime;
     public float startWaitTime;
-
+    Transform target;
     public Transform moveSpot;
     public float minX;
     public float maxX;
     public float minY;
     public float maxY;
+    private SpriteRenderer spriteRenderer;
 
-    void Start()
+    private void Awake()
     {
-        waitTime = startWaitTime;
-        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
 
     }
 
-    void Update()
+    private void Start()
     {
-        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
-        if (Vector2.Distance(transform.position, moveSpot.position) < 0.2f)
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        target = GameObject.Find("PlayerChar").transform;
+    }
+
+    void FixedUpdate()
+    {
+        transform.localPosition = Vector2.MoveTowards(transform.localPosition, moveSpot.localPosition, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.localPosition, moveSpot.localPosition) < 0.2f)
         {
             if (waitTime <= 0)
             {
-                moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                moveSpot.localPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
                 waitTime = startWaitTime;
             }
             else
@@ -36,5 +43,6 @@ public class RandomMovement : MonoBehaviour
                 waitTime -= Time.deltaTime;
             }
         }
+        this.spriteRenderer.flipX = target.transform.localPosition.x > this.transform.localPosition.x;
     }
 }
